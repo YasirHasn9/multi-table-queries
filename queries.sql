e15804cd1c289919bc5bee35edf9c69ddd6c8a52
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-13 17:34:40 -0700

    OrderID, Customer's Company Name and the employee's LastName for every order

--- queries.sql
+++ queries.sql
@@ -5,15 +5,26 @@ select  * from  ProductDetails_V as p;
 select  p.ProductName , c.CategoryName from  ProductDetails_V as p
 join Category as c
 on p.CategoryId = c.id
+
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 select o.id , s.CompanyName from "Order"  as o
 join Shipper as s 
 on o.ShipVia = s.id
 where o.OrderDate < "2012-08-09" ;
+
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 SELECT  p.ProductName , count(*) from "OrderDetail" as o
        join ProductDetails_V as p
 	   on o.ProductId = p.id
 where o.OrderId = 10251
 GROUP by p.ProductName
+
+
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select o.id ,  c.CompanyName  ,  e.LastName from "Order" as o
+    join Customer as c on o.CustomerId = c.id
+	
+	join Employee as e on o.EmployeeId = e.id
